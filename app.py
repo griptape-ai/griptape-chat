04dc44a2793a98a531d0c8d3aab9e86aa4689ec9
alvarodls-gp <alvaro@griptape.ai>
2024-07-08 12:02:25 -0700

    first fixes

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import gradio as gr
 from dotenv import load_dotenv
 from griptape.chat_demo import Chat
 
+#for somereason the program is picky about the names of the files that it takes in, wouldn't take in "Edipo Rey-(bunch of letters and numbers from Anna's archive)-Anna's Archive" but would take the rename "Edipo Rey"?
+
 load_dotenv()
 port = os.getenv("GRADIO_PORT", 7860)
 chat = Chat()
@@ -49,6 +51,6 @@ with gr.Blocks() as demo:
             msg_textbox.submit(user, [msg_textbox, chatbot], [msg_textbox, chatbot], queue=False).then(
                 bot, chatbot, chatbot
             )
-
+#server_name='0.0.0.0', server_port=int(port)
     demo.queue()
-    demo.launch(server_name='0.0.0.0', server_port=int(port))
+    demo.launch(share=True)
--- griptape/chat_demo/chat.py
+++ griptape/chat_demo/chat.py
@@ -1,66 +1,90 @@
 import os
 from tempfile import TemporaryFile
 from attr import define, field, Factory
-from griptape.core import BaseTool
-from griptape.drivers import OpenAiPromptDriver
+from typing import Any
+from dotenv import load_dotenv
+
+
+from griptape.tools import BaseTool
+from griptape.drivers import OpenAiChatPromptDriver,LocalVectorStoreDriver,OpenAiEmbeddingDriver
 from griptape.engines import VectorQueryEngine
 from griptape.loaders import PdfLoader
-from griptape.memory.structure import ConversationMemory
 from griptape.rules import Ruleset, Rule
 from griptape.structures import Agent
-from griptape.tools import KnowledgeBaseClient
+from griptape.tools import VectorStoreClient
+from griptape.config import OpenAiStructureConfig
+from griptape.utils import load_file
 
 
+load_dotenv()
+#not sure if this config stuff is neccessary 
+config = OpenAiStructureConfig()
+config.prompt_driver = OpenAiChatPromptDriver(
+   
+    model="gpt-4o",
+)
+
+    
 @define
 class Chat:
+    open_ai_embedding_driver = OpenAiEmbeddingDriver ()
+    vector_store_driver= LocalVectorStoreDriver (embedding_driver=open_ai_embedding_driver)
+    open_ai_prompt_driver= OpenAiChatPromptDriver(model="gpt-4o")
+
+ 
+    
     agent: Agent = field(
-        default=Factory(
-            lambda self: self.create_agent(ConversationMemory(), []),
+        default=Factory( 
+            lambda self: self.pdf_interact_w_Agent([]),
+           
             takes_self=True
         )
     )
-    query_engine: VectorQueryEngine = field(
-        default=Factory(lambda: VectorQueryEngine()),
-        kw_only=True
+   
+
+    vector_query_engine= VectorQueryEngine(
+        prompt_driver=open_ai_prompt_driver,
+        vector_store_driver= vector_store_driver,
     )
 
-    def send_message(self, message: str) -> str:
+    def send_message(self, message: str) -> Any:
         return self.agent.run(message).output.value
-
+    
     def upload_pdf(self, file: TemporaryFile) -> str:
+        loaded_file= PdfLoader().load(load_file(file.name))
         namespace = os.path.basename(file.name)
+        
+        
 
-        self.query_engine.vector_store_driver.upsert_text_artifacts(
-            {
-                namespace: PdfLoader().load(file.name)
-            }
+        self.vector_query_engine.vector_store_driver.upsert_text_artifacts(
+         {namespace: loaded_file}
         )
-
-        kb_client = KnowledgeBaseClient(
+        
+        vector_store_tool = VectorStoreClient (
+    
             description=f"Contains information about a PDF with name {namespace}.",
-            query_engine=self.query_engine,
-            namespace=namespace
-        )
+            query_engine=self.vector_query_engine,
+            namespace=namespace,
+        ) 
 
-        self.agent = self.create_agent(
-            self.agent.memory,
-            [kb_client]
+        self.agent = self.pdf_interact_w_Agent(
+            [vector_store_tool]
         )
-
+            
         return file.name
 
-    def create_agent(self, memory: ConversationMemory, tools: list[BaseTool]) -> Agent:
+    
+    def pdf_interact_w_Agent(self,  tools: list[BaseTool]) -> Agent:
         return Agent(
-            prompt_driver=OpenAiPromptDriver(model="gpt-4"),
-            memory=memory,
+            prompt_driver=OpenAiChatPromptDriver(model="gpt-4o"),
             tools=tools,
-            rulesets=[
-                Ruleset(
+            rulesets=[Ruleset(
                     name="PDF Chat Assistant",
                     rules=[
                         Rule("Only chat about things in the PDF file that you have access to"),
                         Rule("If you don't have access to a PDF file say that you don't have access to the PDF file")
-                    ]
+                    ] 
                 )
             ]
         )
+   
