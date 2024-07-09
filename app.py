9d703ca74d143fcf5d311ad9fcc90eac313a8c96
alvarodls-gp <alvaro@griptape.ai>
2024-07-09 14:45:51 -0700

    feedback

--- app.py
+++ app.py
@@ -4,16 +4,13 @@ import gradio as gr
 from dotenv import load_dotenv
 from griptape.chat_demo import Chat
 
-#for somereason the program is picky about the names of the files that it takes in, wouldn't take in "Edipo Rey-(bunch of letters and numbers from Anna's archive)-Anna's Archive" but would take the rename "Edipo Rey"?
-
 load_dotenv()
 port = os.getenv("GRADIO_PORT", 7860)
 chat = Chat()
-print('hello')
+
 
 def user(user_message, history):
     return "", history + [[user_message, None]]
-
 
 def bot(history):
     response = chat.send_message(history[-1][0])
@@ -25,7 +22,6 @@ def bot(history):
         time.sleep(0.005)
 
         yield history
-
 
 with gr.Blocks() as demo:
     with gr.Row():
@@ -51,6 +47,5 @@ with gr.Blocks() as demo:
             msg_textbox.submit(user, [msg_textbox, chatbot], [msg_textbox, chatbot], queue=False).then(
                 bot, chatbot, chatbot
             )
-#server_name='0.0.0.0', server_port=int(port)
     demo.queue()
     demo.launch(share=True)
--- griptape/chat_demo/chat.py
+++ griptape/chat_demo/chat.py
@@ -4,44 +4,29 @@ from attr import define, field, Factory
 from typing import Any
 from dotenv import load_dotenv
 
-
-from griptape.tools import BaseTool
 from griptape.drivers import OpenAiChatPromptDriver,LocalVectorStoreDriver,OpenAiEmbeddingDriver
 from griptape.engines import VectorQueryEngine
 from griptape.loaders import PdfLoader
 from griptape.rules import Ruleset, Rule
 from griptape.structures import Agent
-from griptape.tools import VectorStoreClient
-from griptape.config import OpenAiStructureConfig
+from griptape.tools import VectorStoreClient, BaseTool
 from griptape.utils import load_file
 
-
 load_dotenv()
-#not sure if this config stuff is neccessary 
-config = OpenAiStructureConfig()
-config.prompt_driver = OpenAiChatPromptDriver(
-   
-    model="gpt-4o",
-)
 
-    
 @define
 class Chat:
     open_ai_embedding_driver = OpenAiEmbeddingDriver ()
     vector_store_driver= LocalVectorStoreDriver (embedding_driver=open_ai_embedding_driver)
     open_ai_prompt_driver= OpenAiChatPromptDriver(model="gpt-4o")
 
- 
-    
     agent: Agent = field(
         default=Factory( 
             lambda self: self.pdf_interact_w_Agent([]),
-           
             takes_self=True
         )
     )
    
-
     vector_query_engine= VectorQueryEngine(
         prompt_driver=open_ai_prompt_driver,
         vector_store_driver= vector_store_driver,
@@ -54,26 +39,19 @@ class Chat:
         loaded_file= PdfLoader().load(load_file(file.name))
         namespace = os.path.basename(file.name)
         
-        
-
         self.vector_query_engine.vector_store_driver.upsert_text_artifacts(
          {namespace: loaded_file}
         )
-        
         vector_store_tool = VectorStoreClient (
-    
             description=f"Contains information about a PDF with name {namespace}.",
             query_engine=self.vector_query_engine,
             namespace=namespace,
         ) 
-
         self.agent = self.pdf_interact_w_Agent(
             [vector_store_tool]
         )
-            
         return file.name
 
-    
     def pdf_interact_w_Agent(self,  tools: list[BaseTool]) -> Agent:
         return Agent(
             prompt_driver=OpenAiChatPromptDriver(model="gpt-4o"),
