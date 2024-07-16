847e8f20ecf102ab5594b2cc79c6bc486f1cb554
alvarodls-gp <alvaro@griptape.ai>
2024-07-16 15:11:21 -0700

    cloud-agent-compatibility

--- app.py
+++ app.py
@@ -13,39 +13,32 @@ def user(user_message, history):
 
 def bot(history):
     response = chat.send_message(history[-1][0])
-    print(response)
     history[-1][1] = ""
 
     for character in response:
         history[-1][1] += character
-
         time.sleep(0.005)
 
         yield history
 
 with gr.Blocks() as demo:
     with gr.Row():
+
         with gr.Column(scale=1):
-            gr.Label("Griptape PDF Chat", show_label=False)
+            gr.Label("Griptape Chat", show_label=False)
             gr.Text(
                 "Before using chat, do the following:\n\n1. Make the OpenAI API token available via an environment "
-                "variable OPENAI_API_KEY\n2. Upload a PDF file that you'd like to chat with",
+                "variable OPENAI_API_KEY", 
                 show_label=False
-            )
-            file_output = gr.File()
-            upload_btn = gr.UploadButton(
-                variant="primary",
-                file_types=[".pdf"],
-                file_count="single"
             )
         with gr.Column(scale=3):
             chatbot = gr.Chatbot(show_label=False)
-            msg_textbox = gr.Textbox(placeholder="Send a message", show_label=False)
 
-            upload_btn.upload(chat.upload_pdf,upload_btn, file_output)
+            msg_textbox = gr.Textbox(placeholder="Send a message", show_label=False)
 
             msg_textbox.submit(user, [msg_textbox, chatbot], [msg_textbox, chatbot], queue=False).then(
                 bot, chatbot, chatbot
             )
+            
     demo.queue()
     demo.launch(share=True)
--- griptape/chat_demo/chat.py
+++ griptape/chat_demo/chat.py
@@ -5,12 +5,7 @@ from typing import Any
 from dotenv import load_dotenv
 
 from griptape.drivers import LocalVectorStoreDriver,OpenAiEmbeddingDriver,GriptapeCloudStructureRunDriver
-from griptape.loaders import PdfLoader
-from griptape.rules import Ruleset, Rule
-from griptape.structures import Agent
-from griptape.tools import VectorStoreClient, BaseTool
-from griptape.tasks import StructureRunTask
-from griptape.utils import load_file
+from griptape.artifacts import TextArtifact
 
 load_dotenv()
 
@@ -18,79 +13,11 @@ load_dotenv()
 class Chat:
     open_ai_embedding_driver = OpenAiEmbeddingDriver ()
     vector_store_driver= LocalVectorStoreDriver (embedding_driver=open_ai_embedding_driver)
-
-    struct_run_task: StructureRunTask = field(
-        default=Factory(
-            lambda: StructureRunTask(
-                driver=GriptapeCloudStructureRunDriver(
-                    base_url=os.environ["GT_CLOUD_BASE_URL"],
-                    api_key=os.environ["GT_CLOUD_API_KEY"],
-                    structure_id=os.environ["GT_STRUCTURE_ID"],
-                ),
-            ),
-        
-        
-        ),
-        
-        kw_only=True,
-    )
-
-    
-
-   
     def send_message( self, message: str) -> Any:
-        self.struct_run_task.input=(message, )
-        return self.struct_run_task.run().value
-    def upload_pdf(self,file:TemporaryFile)-> str:
-        return file.name
-        
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    #this is old PDF stuff, now time to get started on convo memory 
-
-    '''
-    def upload_pdf(self, file: TemporaryFile) -> str:
-        # loaded_file= PdfLoader().load(load_file(file.name))
-        # namespace = os.path.basename(file.name)
-        # self.vector_store_driver.upsert_text_artifacts(
-        #     {namespace: loaded_file}
-        # )
-        # vector_store_tool = VectorStoreClient (
-        #     description=f"Contains information about a PDF with name {namespace}.",
-        #     vector_store_driver=self.vector_store_driver,
-        #     query_params={namespace:loaded_file},
-        # )
-        # self.struct_run_task= self.pdf_interact_with_Agent(
-        #     [vector_store_tool]
-        # )
-        return file.name
-    
-
-    def pdf_interact_with_Agent(self,  tools: list[BaseTool]) -> Agent:
-        return Agent(
-            tools=tools,
-            rulesets=[Ruleset(
-                    name="PDF Chat Assistant",
-                    rules=[
-                        Rule("Only chat about things in the PDF file that you have access to"),
-                        Rule("If you don't have access to a PDF file say that you don't have access to the PDF file")
-                    ] 
-                )
-            ]
-        )
-   '''
+        driver=GriptapeCloudStructureRunDriver(
+                base_url=os.environ["GT_CLOUD_BASE_URL"],
+                api_key=os.environ["GT_CLOUD_API_KEY"],
+                structure_id=os.environ["GT_STRUCTURE_ID"],
+            )
+        return driver.run(TextArtifact(message)).value
+   
